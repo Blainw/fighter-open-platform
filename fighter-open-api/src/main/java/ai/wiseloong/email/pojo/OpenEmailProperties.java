ce37e33caf51948d681831b05dd55dfb1fa73495
dengxuefeng <dxf645478168@qq.com>
2022-07-01 15:11:37 +0800

    email

--- fighter-open-api/src/main/java/ai/wiseloong/email/pojo/OpenEmailProperties.java
+++ fighter-open-api/src/main/java/ai/wiseloong/email/pojo/OpenEmailProperties.java
@@ -4,11 +4,6 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-/**
- * Fighter框架的配置文件使用到的属性
- *
- * @author 张晋荣 <a href="mailto:phenom_work@163.com">点此联系我</a>
- */
 @Data
 @ConfigurationProperties(prefix = "spring.mail")
 @Component
